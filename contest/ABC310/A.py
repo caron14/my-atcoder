cb6697426b1dfd2f7b3990830cff1ad31bca12ee
caron14 <supika.caron@gmail.com>
2023-07-16 12:37:19 +0900

    AC

--- contest/ABC310/A.py
+++ contest/ABC310/A.py
@@ -7,6 +7,5 @@ def main():
 
     return min(tot)
 
-
 if __name__=='__main__':
     print(main())
--- contest/ABC310/C.py
+++ contest/ABC310/C.py
@@ -1,27 +1,10 @@
-def are_strings_equal(string1, string2):
-    return hash(string1) == hash(string2)
-
-
 def main():
     N = int(input())
-    S, S_r = [], []
+    S = set()
     for _ in range(N):
         s = input()
-        S.append(s)
-        S_r.append(s[::-1])
-
-    cnt = 0
-    for i in range(N):
-        for j in range(i+1, N):
-            # if S[i] == S[j]:
-            if are_strings_equal(S[i], S[j]):
-                break
-            # elif S[i] == S_r[j]:
-            elif are_strings_equal(S[i], S_r[j]):
-                break
-        else:
-            cnt += 1
-    return cnt
+        S.add(min(s, s[::-1]))
+    return len(S)
 
 if __name__ == '__main__':
     print(main())
